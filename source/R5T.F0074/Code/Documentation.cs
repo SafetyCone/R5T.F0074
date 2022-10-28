550726e0f6d569290f614265ea5c442ef910bbd2
davidcoats <david@getsafetycone.com>
2022-10-28 12:03:09 -0700

    Initial commit.

---
+++ source/R5T.F0074/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0074
+{
+	/// <summary>
+	/// AWS S3 functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.F0074/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.F0074
+{
+    public static class Instances
+    {
+		
+    }
+}
